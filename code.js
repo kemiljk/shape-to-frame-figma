7cc1702b66b2efbce33956ae10efd0bec037668b
Karl Koch <karlkoch@Karls-MacBook-Pro.local>
2021-07-14 10:22:22 +0100

    feat: you can convert shapes back to frames too

--- code.js
+++ code.js
@@ -9,32 +9,60 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 const { selection } = figma.currentPage;
 const nodes = [];
+const nodetypes = ["RECTANGLE", "ELLIPSE"];
 function getStyles() {
     return __awaiter(this, void 0, void 0, function* () {
         figma.root.children.flatMap((pageNode) => pageNode.selection.forEach((node) => __awaiter(this, void 0, void 0, function* () {
-            const frame = figma.createFrame();
-            frame.name = node.name;
-            if (node.cornerRadius !== figma.mixed) {
-                frame.cornerRadius = node.cornerRadius;
+            if (nodetypes.includes(node.type)) {
+                const frame = figma.createFrame();
+                frame.name = node.name;
+                if (node.cornerRadius !== figma.mixed) {
+                    frame.cornerRadius = node.cornerRadius;
+                }
+                else {
+                    frame.topLeftRadius = node.topLeftRadius;
+                    frame.topRightRadius = node.topRightRadius;
+                    frame.bottomLeftRadius = node.bottomLeftRadius;
+                    frame.bottomRightRadius = node.bottomRightRadius;
+                }
+                if (node.type === "ELLIPSE") {
+                    frame.cornerRadius = frame.width * 2;
+                }
+                frame.fills = node.fills;
+                frame.strokes = node.strokes;
+                frame.strokeWeight = node.strokeWeight;
+                frame.resize(node.width, node.height);
+                frame.effects = node.effects;
+                frame.x = node.x;
+                frame.y = node.y;
+                nodes.push(frame);
+                node.remove();
             }
-            else {
-                frame.topLeftRadius = node.topLeftRadius;
-                frame.topRightRadius = node.topRightRadius;
-                frame.bottomLeftRadius = node.bottomLeftRadius;
-                frame.bottomRightRadius = node.bottomRightRadius;
+            else if (node.type === "FRAME") {
+                const shape = figma.createRectangle();
+                shape.name = node.name;
+                if (node.cornerRadius !== figma.mixed) {
+                    shape.cornerRadius = node.cornerRadius;
+                }
+                else {
+                    shape.topLeftRadius = node.topLeftRadius;
+                    shape.topRightRadius = node.topRightRadius;
+                    shape.bottomLeftRadius = node.bottomLeftRadius;
+                    shape.bottomRightRadius = node.bottomRightRadius;
+                }
+                if (node.cornerRadius >= node.width * 2) {
+                    shape.cornerRadius = shape.width * 2;
+                }
+                shape.fills = node.fills;
+                shape.strokes = node.strokes;
+                shape.strokeWeight = node.strokeWeight;
+                shape.resize(node.width, node.height);
+                shape.effects = node.effects;
+                shape.x = node.x;
+                shape.y = node.y;
+                nodes.push(shape);
+                node.remove();
             }
-            if (node.type === "ELLIPSE") {
-                frame.cornerRadius = frame.width * 2;
-            }
-            frame.fills = node.fills;
-            frame.strokes = node.strokes;
-            frame.strokeWeight = node.strokeWeight;
-            frame.resize(node.width, node.height);
-            frame.effects = node.effects;
-            frame.x = node.x;
-            frame.y = node.y;
-            nodes.push(frame);
-            node.remove();
         })));
         figma.currentPage.selection = nodes;
         figma.viewport.scrollAndZoomIntoView(nodes);
--- code.ts
+++ code.ts
@@ -1,31 +1,57 @@
 const { selection } = figma.currentPage;
 const nodes: SceneNode[] = [];
+const nodetypes = ["RECTANGLE", "ELLIPSE"];
 
 async function getStyles(): Promise<String> {
   figma.root.children.flatMap((pageNode) =>
     pageNode.selection.forEach(async (node) => {
-      const frame = figma.createFrame();
-      frame.name = node.name;
-      if (node.cornerRadius !== figma.mixed) {
-        frame.cornerRadius = node.cornerRadius;
-      } else {
-        frame.topLeftRadius = node.topLeftRadius;
-        frame.topRightRadius = node.topRightRadius;
-        frame.bottomLeftRadius = node.bottomLeftRadius;
-        frame.bottomRightRadius = node.bottomRightRadius;
+      if (nodetypes.includes(node.type)) {
+        const frame = figma.createFrame();
+        frame.name = node.name;
+        if (node.cornerRadius !== figma.mixed) {
+          frame.cornerRadius = node.cornerRadius;
+        } else {
+          frame.topLeftRadius = node.topLeftRadius;
+          frame.topRightRadius = node.topRightRadius;
+          frame.bottomLeftRadius = node.bottomLeftRadius;
+          frame.bottomRightRadius = node.bottomRightRadius;
+        }
+        if (node.type === "ELLIPSE") {
+          frame.cornerRadius = frame.width * 2;
+        }
+        frame.fills = node.fills;
+        frame.strokes = node.strokes;
+        frame.strokeWeight = node.strokeWeight;
+        frame.resize(node.width, node.height);
+        frame.effects = node.effects;
+        frame.x = node.x;
+        frame.y = node.y;
+        nodes.push(frame);
+        node.remove();
+      } else if (node.type === "FRAME") {
+        const shape = figma.createRectangle();
+        shape.name = node.name;
+        if (node.cornerRadius !== figma.mixed) {
+          shape.cornerRadius = node.cornerRadius;
+        } else {
+          shape.topLeftRadius = node.topLeftRadius;
+          shape.topRightRadius = node.topRightRadius;
+          shape.bottomLeftRadius = node.bottomLeftRadius;
+          shape.bottomRightRadius = node.bottomRightRadius;
+        }
+        if (node.cornerRadius >= node.width * 2) {
+          shape.cornerRadius = shape.width * 2;
+        }
+        shape.fills = node.fills;
+        shape.strokes = node.strokes;
+        shape.strokeWeight = node.strokeWeight;
+        shape.resize(node.width, node.height);
+        shape.effects = node.effects;
+        shape.x = node.x;
+        shape.y = node.y;
+        nodes.push(shape);
+        node.remove();
       }
-      if (node.type === "ELLIPSE") {
-        frame.cornerRadius = frame.width * 2;
-      }
-      frame.fills = node.fills;
-      frame.strokes = node.strokes;
-      frame.strokeWeight = node.strokeWeight;
-      frame.resize(node.width, node.height);
-      frame.effects = node.effects;
-      frame.x = node.x;
-      frame.y = node.y;
-      nodes.push(frame);
-      node.remove();
     })
   );
   figma.currentPage.selection = nodes;
