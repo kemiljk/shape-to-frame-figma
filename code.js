3d795112fde344c56b2dcc4b5db21b101cb00bcf
Karl Koch <karl@kejk.tech>
2021-10-26 19:24:10 +0100

    Feat: now adds menu commands and ability to apply auto layout

--- code.js
+++ code.js
@@ -12,66 +12,120 @@ const nodes = [];
 function getStyles() {
     return __awaiter(this, void 0, void 0, function* () {
         figma.currentPage.selection.forEach((node) => __awaiter(this, void 0, void 0, function* () {
-            if (node.type === "RECTANGLE" || node.type === "ELLIPSE") {
-                const frame = figma.createFrame();
-                frame.name = node.name;
-                if (node.cornerRadius !== figma.mixed) {
-                    frame.cornerRadius = node.cornerRadius;
-                }
-                else {
-                    frame.topLeftRadius = node.topLeftRadius;
-                    frame.topRightRadius = node.topRightRadius;
-                    frame.bottomLeftRadius = node.bottomLeftRadius;
-                    frame.bottomRightRadius = node.bottomRightRadius;
-                }
-                if (node.type === "ELLIPSE") {
-                    frame.cornerRadius = frame.width * 2;
-                }
-                frame.fills = node.fills;
-                frame.strokes = node.strokes;
-                frame.strokeWeight = node.strokeWeight;
-                frame.resize(node.width, node.height);
-                frame.effects = node.effects;
-                frame.x = node.x;
-                frame.y = node.y;
-                if (node.parent.type === "FRAME" || node.parent.type === "GROUP") {
-                    node.parent.insertChild(0, frame);
+            if (figma.command === "shape-to-frame") {
+                if (node.type === "RECTANGLE" || node.type === "ELLIPSE") {
+                    const frame = figma.createFrame();
+                    frame.name = node.name;
+                    if (node.cornerRadius !== figma.mixed) {
+                        frame.cornerRadius = node.cornerRadius;
+                    }
+                    else {
+                        frame.topLeftRadius = node.topLeftRadius;
+                        frame.topRightRadius = node.topRightRadius;
+                        frame.bottomLeftRadius = node.bottomLeftRadius;
+                        frame.bottomRightRadius = node.bottomRightRadius;
+                    }
+                    if (node.type === "ELLIPSE") {
+                        frame.cornerRadius = frame.width * 2;
+                    }
+                    frame.fills = node.fills;
+                    frame.strokes = node.strokes;
+                    frame.strokeWeight = node.strokeWeight;
+                    frame.resize(node.width, node.height);
+                    frame.effects = node.effects;
                     frame.x = node.x;
                     frame.y = node.y;
-                }
-                nodes.push(frame);
-                node.remove();
-            }
-            else if (node.type === "FRAME") {
-                const shape = figma.createRectangle();
-                shape.name = node.name;
-                if (node.cornerRadius !== figma.mixed) {
-                    shape.cornerRadius = node.cornerRadius;
+                    if (node.parent.type === "FRAME" || node.parent.type === "GROUP") {
+                        node.parent.insertChild(0, frame);
+                        frame.x = node.x;
+                        frame.y = node.y;
+                    }
+                    nodes.push(frame);
+                    node.remove();
+                    figma.notify("Converted!");
                 }
                 else {
-                    shape.topLeftRadius = node.topLeftRadius;
-                    shape.topRightRadius = node.topRightRadius;
-                    shape.bottomLeftRadius = node.bottomLeftRadius;
-                    shape.bottomRightRadius = node.bottomRightRadius;
+                    figma.notify("Please select a shape!");
                 }
-                if (node.cornerRadius !== figma.mixed &&
-                    node.cornerRadius >= node.width * 2) {
-                    shape.cornerRadius = shape.width * 2;
-                }
-                shape.fills = node.fills;
-                shape.strokes = node.strokes;
-                shape.strokeWeight = node.strokeWeight;
-                shape.resize(node.width, node.height);
-                shape.effects = node.effects;
-                shape.x = node.x;
-                shape.y = node.y;
-                if (node.parent.type === "FRAME" || node.parent.type === "GROUP") {
-                    node.parent.insertChild(0, shape);
+            }
+            if (figma.command === "frame-to-shape") {
+                if (node.type === "FRAME") {
+                    const shape = figma.createRectangle();
+                    shape.name = node.name;
+                    if (node.cornerRadius !== figma.mixed) {
+                        shape.cornerRadius = node.cornerRadius;
+                    }
+                    else {
+                        shape.topLeftRadius = node.topLeftRadius;
+                        shape.topRightRadius = node.topRightRadius;
+                        shape.bottomLeftRadius = node.bottomLeftRadius;
+                        shape.bottomRightRadius = node.bottomRightRadius;
+                    }
+                    if (node.cornerRadius !== figma.mixed &&
+                        node.cornerRadius >= node.width * 2) {
+                        shape.cornerRadius = shape.width * 2;
+                    }
+                    shape.fills = node.fills;
+                    shape.strokes = node.strokes;
+                    shape.strokeWeight = node.strokeWeight;
+                    shape.resize(node.width, node.height);
+                    shape.effects = node.effects;
                     shape.x = node.x;
                     shape.y = node.y;
+                    if (node.parent.type === "FRAME" || node.parent.type === "GROUP") {
+                        node.parent.insertChild(0, shape);
+                        shape.x = node.x;
+                        shape.y = node.y;
+                    }
+                    nodes.push(shape);
+                    node.remove();
+                    figma.notify("Converted!");
                 }
-                nodes.push(shape);
-                node.remove();
+                else {
+                    figma.notify("Please select a Frame!");
+                }
+            }
+            if (figma.command === "shape-to-al-frame") {
+                if (node.type === "RECTANGLE" || node.type === "ELLIPSE") {
+                    const frame = figma.createFrame();
+                    frame.name = node.name;
+                    if (node.cornerRadius !== figma.mixed) {
+                        frame.cornerRadius = node.cornerRadius;
+                    }
+                    else {
+                        frame.topLeftRadius = node.topLeftRadius;
+                        frame.topRightRadius = node.topRightRadius;
+                        frame.bottomLeftRadius = node.bottomLeftRadius;
+                        frame.bottomRightRadius = node.bottomRightRadius;
+                    }
+                    if (node.type === "ELLIPSE") {
+                        frame.cornerRadius = frame.width * 2;
+                    }
+                    frame.fills = node.fills;
+                    frame.strokes = node.strokes;
+                    frame.strokeWeight = node.strokeWeight;
+                    frame.resize(node.width, node.height);
+                    frame.effects = node.effects;
+                    frame.x = node.x;
+                    frame.y = node.y;
+                    frame.layoutMode = "HORIZONTAL";
+                    frame.itemSpacing = 8;
+                    frame.paddingTop = 0;
+                    frame.paddingLeft = 0;
+                    frame.paddingBottom = 0;
+                    frame.paddingRight = 0;
+                    if (node.parent.type === "FRAME" || node.parent.type === "GROUP") {
+                        node.parent.insertChild(0, frame);
+                        frame.x = node.x;
+                        frame.y = node.y;
+                    }
+                    nodes.push(frame);
+                    node.remove();
+                    figma.notify("Converted!");
+                }
+                else {
+                    figma.notify("Please select a Frame!");
+                }
             }
         }));
         figma.currentPage.selection = nodes;
@@ -80,5 +134,4 @@ function getStyles() {
     });
 }
 getStyles();
-figma.notify("Converted!");
 figma.closePlugin();
--- code.ts
+++ code.ts
@@ -3,65 +3,116 @@ const nodes: SceneNode[] = [];
 
 async function getStyles(): Promise<String> {
   figma.currentPage.selection.forEach(async (node) => {
-    if (node.type === "RECTANGLE" || node.type === "ELLIPSE") {
-      const frame = figma.createFrame();
-      frame.name = node.name;
-      if (node.cornerRadius !== figma.mixed) {
-        frame.cornerRadius = node.cornerRadius;
-      } else {
-        frame.topLeftRadius = (node as RectangleNode).topLeftRadius;
-        frame.topRightRadius = (node as RectangleNode).topRightRadius;
-        frame.bottomLeftRadius = (node as RectangleNode).bottomLeftRadius;
-        frame.bottomRightRadius = (node as RectangleNode).bottomRightRadius;
-      }
-      if (node.type === "ELLIPSE") {
-        frame.cornerRadius = frame.width * 2;
-      }
-      frame.fills = node.fills;
-      frame.strokes = node.strokes;
-      frame.strokeWeight = node.strokeWeight;
-      frame.resize(node.width, node.height);
-      frame.effects = node.effects;
-      frame.x = node.x;
-      frame.y = node.y;
-      if (node.parent.type === "FRAME" || node.parent.type === "GROUP") {
-        node.parent.insertChild(0, frame);
+    if (figma.command === "shape-to-frame") {
+      if (node.type === "RECTANGLE" || node.type === "ELLIPSE") {
+        const frame = figma.createFrame();
+        frame.name = node.name;
+        if (node.cornerRadius !== figma.mixed) {
+          frame.cornerRadius = node.cornerRadius;
+        } else {
+          frame.topLeftRadius = (node as RectangleNode).topLeftRadius;
+          frame.topRightRadius = (node as RectangleNode).topRightRadius;
+          frame.bottomLeftRadius = (node as RectangleNode).bottomLeftRadius;
+          frame.bottomRightRadius = (node as RectangleNode).bottomRightRadius;
+        }
+        if (node.type === "ELLIPSE") {
+          frame.cornerRadius = frame.width * 2;
+        }
+        frame.fills = node.fills;
+        frame.strokes = node.strokes;
+        frame.strokeWeight = node.strokeWeight;
+        frame.resize(node.width, node.height);
+        frame.effects = node.effects;
         frame.x = node.x;
         frame.y = node.y;
-      }
-      nodes.push(frame);
-      node.remove();
-    } else if (node.type === "FRAME") {
-      const shape = figma.createRectangle();
-      shape.name = node.name;
-      if (node.cornerRadius !== figma.mixed) {
-        shape.cornerRadius = node.cornerRadius;
+        if (node.parent.type === "FRAME" || node.parent.type === "GROUP") {
+          node.parent.insertChild(0, frame);
+          frame.x = node.x;
+          frame.y = node.y;
+        }
+        nodes.push(frame);
+        node.remove();
+        figma.notify("Converted!");
       } else {
-        shape.topLeftRadius = node.topLeftRadius;
-        shape.topRightRadius = node.topRightRadius;
-        shape.bottomLeftRadius = node.bottomLeftRadius;
-        shape.bottomRightRadius = node.bottomRightRadius;
+        figma.notify("Please select a shape!");
       }
-      if (
-        node.cornerRadius !== figma.mixed &&
-        node.cornerRadius >= node.width * 2
-      ) {
-        shape.cornerRadius = shape.width * 2;
-      }
-      shape.fills = node.fills;
-      shape.strokes = node.strokes;
-      shape.strokeWeight = node.strokeWeight;
-      shape.resize(node.width, node.height);
-      shape.effects = node.effects;
-      shape.x = node.x;
-      shape.y = node.y;
-      if (node.parent.type === "FRAME" || node.parent.type === "GROUP") {
-        node.parent.insertChild(0, shape);
+    }
+    if (figma.command === "frame-to-shape") {
+      if (node.type === "FRAME") {
+        const shape = figma.createRectangle();
+        shape.name = node.name;
+        if (node.cornerRadius !== figma.mixed) {
+          shape.cornerRadius = node.cornerRadius;
+        } else {
+          shape.topLeftRadius = node.topLeftRadius;
+          shape.topRightRadius = node.topRightRadius;
+          shape.bottomLeftRadius = node.bottomLeftRadius;
+          shape.bottomRightRadius = node.bottomRightRadius;
+        }
+        if (
+          node.cornerRadius !== figma.mixed &&
+          node.cornerRadius >= node.width * 2
+        ) {
+          shape.cornerRadius = shape.width * 2;
+        }
+        shape.fills = node.fills;
+        shape.strokes = node.strokes;
+        shape.strokeWeight = node.strokeWeight;
+        shape.resize(node.width, node.height);
+        shape.effects = node.effects;
         shape.x = node.x;
         shape.y = node.y;
+        if (node.parent.type === "FRAME" || node.parent.type === "GROUP") {
+          node.parent.insertChild(0, shape);
+          shape.x = node.x;
+          shape.y = node.y;
+        }
+        nodes.push(shape);
+        node.remove();
+        figma.notify("Converted!");
+      } else {
+        figma.notify("Please select a Frame!");
       }
-      nodes.push(shape);
-      node.remove();
+    }
+    if (figma.command === "shape-to-al-frame") {
+      if (node.type === "RECTANGLE" || node.type === "ELLIPSE") {
+        const frame = figma.createFrame();
+        frame.name = node.name;
+        if (node.cornerRadius !== figma.mixed) {
+          frame.cornerRadius = node.cornerRadius;
+        } else {
+          frame.topLeftRadius = (node as RectangleNode).topLeftRadius;
+          frame.topRightRadius = (node as RectangleNode).topRightRadius;
+          frame.bottomLeftRadius = (node as RectangleNode).bottomLeftRadius;
+          frame.bottomRightRadius = (node as RectangleNode).bottomRightRadius;
+        }
+        if (node.type === "ELLIPSE") {
+          frame.cornerRadius = frame.width * 2;
+        }
+        frame.fills = node.fills;
+        frame.strokes = node.strokes;
+        frame.strokeWeight = node.strokeWeight;
+        frame.resize(node.width, node.height);
+        frame.effects = node.effects;
+        frame.x = node.x;
+        frame.y = node.y;
+        frame.layoutMode = "HORIZONTAL";
+        frame.itemSpacing = 8;
+        frame.paddingTop = 0;
+        frame.paddingLeft = 0;
+        frame.paddingBottom = 0;
+        frame.paddingRight = 0;
+        if (node.parent.type === "FRAME" || node.parent.type === "GROUP") {
+          node.parent.insertChild(0, frame);
+          frame.x = node.x;
+          frame.y = node.y;
+        }
+        nodes.push(frame);
+        node.remove();
+        figma.notify("Converted!");
+      } else {
+        figma.notify("Please select a Frame!");
+      }
     }
   });
   figma.currentPage.selection = nodes;
@@ -70,6 +121,5 @@ async function getStyles(): Promise<String> {
 }
 
 getStyles();
-figma.notify("Converted!");
 
 figma.closePlugin();
